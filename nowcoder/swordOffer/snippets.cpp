6ee9e93566c48ea0369fde5db9125920a5c76558
Lambda <2861429309@qq.com>
2018-06-05 21:46:38 +0800

    modify swordOffer snippets

--- nowcoder/swordOffer/snippets.cpp
+++ nowcoder/swordOffer/snippets.cpp
@@ -2,3 +2,35 @@
 
 // 打印消息
 std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+
+// 快速排序
+template <class ForwardIt>
+void quicksort(ForwardIt first, ForwardIt last)
+{
+    if(first == last) return;
+    auto pivot = *std::next(first, std::distance(first,last)/2);
+    ForwardIt middle1 = std::partition(first, last, 
+                            [pivot](const auto& em){ return em < pivot; });
+    ForwardIt middle2 = std::partition(middle1, last, 
+                            [pivot](const auto& em){ return !(pivot < em); });
+    quicksort(first, middle1);
+    quicksort(middle2, last);
+}
+
+// 归并排序
+template<class Iter>
+void merge_sort(Iter first, Iter last)
+{
+    if (last - first > 1) {
+        Iter middle = first + (last - first) / 2;
+        merge_sort(first, middle);
+        merge_sort(middle, last);
+        std::inplace_merge(first, middle, last);
+    }
+}
+
+// 插入排序
+// insertion sort
+for (auto i = v.begin(); i != v.end(); ++i) {
+    std::rotate(std::upper_bound(v.begin(), i, *i), i, i+1);
+}
