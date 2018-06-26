b9fcccc029fa9a02e56413c660aa179edc9a88d7
LambdaRan <272045023@qq.com>
2018-06-27 00:33:54 +0800

    swordOffer problem 29 writting

---
+++ nowcoder/swordOffer/prob29.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <algorithm>
+
+
+using namespace std;
+
+/*
+* Name: 最小的K个数
+* Description: 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+*
+*/
+class Solution {
+public:
+    vector<int> GetLeastNumbers_Solution(vector<int> input, int k) 
+    {
+        int size = static_cast<int>(input.size());
+        
+    }
+};
+
+int main() 
+{
+    Solution s;
+
+    std::cout << "method 1: \n";
+
+    std::cout << std::endl;
+    return 0;
+}
