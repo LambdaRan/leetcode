bf9bcec48f83db43822723bcac499025c6cc20e2
Lambda <2861429309@qq.com>
2018-06-27 20:02:39 +0800

    29 error

--- nowcoder/swordOffer/prob29.cpp
+++ nowcoder/swordOffer/prob29.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <algorithm>
 
+#include <vector>
+#include <iterator>
 
 using namespace std;
 
@@ -12,18 +14,65 @@ using namespace std;
 */
 class Solution {
 public:
+    vector<int> GetLeastNumbers_Solution_Algorithm(vector<int> input, int k) 
+    {
+        int size = static_cast<int>(input.size());
+        if (size < 0 || k < 0 || size < k) return vector<int>();
+        std::partial_sort(input.begin(), input.begin()+k, input.end());
+        return vector<int>(input.begin(), input.begin()+k);
+    }
+
     vector<int> GetLeastNumbers_Solution(vector<int> input, int k) 
     {
         int size = static_cast<int>(input.size());
+        if (size < 0 || k < 0 || size < k) return vector<int>();
         
-    }
+        vector<int>::iterator first = input.begin();
+        vector<int>::iterator last = input.end();
+        while (first < last)
+        {
+            int pivot = *std::next(input.begin(), std::distance(first, last)/2);
+            std::cout << "pivot: " << pivot << "\n";
+            vector<int>::iterator middle = std::partition(input.begin(), input.end(), 
+                                            [pivot](const int em){ return em < pivot; });
+            int dist = static_cast<int>(std::distance(input.begin(), middle));
+            std::cout << "dist: " << dist << "\n";
+            if (dist == k)
+                break;
+            else if (dist < k)
+                first = std::next(input.begin(), dist+1);
+            else  
+                last = std::next(input.begin(), dist-1);
+            std::cout << "first: " << *first << "mid: " << *middle << "\n";
+
+            break;
+        }
+        return vector<int>(input.begin(), std::next(input.begin(), k));
+    }    
 };
 
 int main() 
 {
     Solution s;
+    // vector<int> vec = {4,5,1,6,2,7,3,8};
+    // vector<int> result = s.GetLeastNumbers_Solution(vec, 4);
+    // std::copy(result.begin(), result.end(), std::ostream_iterator<int>(std::cout, " "));
+    // std::cout << "\n";
 
-    std::cout << "method 1: \n";
+    // vector<int> vec1 = {4};
+    // result = s.GetLeastNumbers_Solution(vec1, 1);
+    // std::copy(result.begin(), result.end(), std::ostream_iterator<int>(std::cout, " "));
+    // std::cout << "\n";
+
+    // vector<int> vec2 = {4,5};
+    // result = s.GetLeastNumbers_Solution(vec2, 2);
+    // std::copy(result.begin(), result.end(), std::ostream_iterator<int>(std::cout, " "));
+    // std::cout << "\n";
+
+    vector<int> vec3 = {4,5,1,2,3,2,1,2,2,2};
+    vector<int> result = s.GetLeastNumbers_Solution(vec3, 5);
+    std::copy(result.begin(), result.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << "\n";
 
     std::cout << std::endl;
     return 0;
