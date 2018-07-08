c82b10df22078f696f1294a8f4cade6faeebd122
LambdaRan <272045023@qq.com>
2018-07-09 00:25:27 +0800

    swordOffer problem 49 ok

--- nowcoder/swordOffer/prob49.cpp
+++ nowcoder/swordOffer/prob49.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <string>
 
+#include <cctype>
+#include <stdio.h>
+
 using namespace std;
 
 /*
@@ -25,8 +28,43 @@ using namespace std;
 */
 class Solution {
 public:
-    bool function() {
-        
+    int StrToInt(string str) {
+        if (str.empty()) return 0;
+        const int INT_MAX = 2147483647;
+        const int INT_MIN = -2147483648;
+        int index = 0;
+        int c;
+        int neg = 0;
+        int ret = 0;
+        do { /* 清除开头的空格 */
+            c = str[index++];
+        } while(std::isspace(c));
+        /* 判断符号 */
+        if (c == '-') 
+        {
+            neg = 1;
+            c = str[index++];
+        } else {
+            neg = 0;
+            if (c == '+')
+                c = str[index++];
+        }
+        for ( ; std::isdigit(c); c = str[index++]) 
+        {
+            int digit = c - '0';
+            if (neg) {
+                if (-ret < (INT_MIN + digit)/10)
+                return INT_MIN;
+            } else {
+                if (ret > (INT_MAX - digit)/10)
+                return INT_MAX;
+            }
+
+            ret = ret * 10 + digit;
+        }
+        if (index < static_cast<int>(str.size()))
+            return 0;
+        return neg ? -ret : ret;
     }
 };
 
@@ -34,7 +72,16 @@ int main()
 {
     Solution s;
 
-    std::cout << "method 1: \n";
+    printf("\"%s\" = %d\n", "123", s.StrToInt("123"));
+    printf("\"%s\" = %d\n", "   123", s.StrToInt("    123"));
+    printf("\"%s\" = %d\n", "+123", s.StrToInt("+123"));
+    printf("\"%s\" = %d\n", " -123", s.StrToInt(" -123"));
+    printf("\"%s\" = %d\n", "123ABC", s.StrToInt("123ABC"));
+    printf("\"%s\" = %d\n", " abc123ABC", s.StrToInt(" abc123ABC"));
+    printf("\"%s\" = %d\n", "2147483647", s.StrToInt("2147483647"));
+    printf("\"%s\" = %d\n", "-2147483648", s.StrToInt("-2147483648"));
+    printf("\"%s\" = %d\n", "2147483648", s.StrToInt("2147483648"));
+    printf("\"%s\" = %d\n", "-2147483649", s.StrToInt("-2147483649"));
 
     std::cout << std::endl;
     return 0;
--- nowcoder/swordOffer/test.cpp
+++ nowcoder/swordOffer/test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <vector>
+#include <limits>
 #include <algorithm>
 
 using namespace std;
 
 int main()
 {
-
+    std::cout << std::numeric_limits<int>::max() << std::endl;
+    std::cout << std::numeric_limits<int>::max_digits10 << std::endl;
+    std::cout << std::numeric_limits<int>::digits10 << std::endl;
 }
