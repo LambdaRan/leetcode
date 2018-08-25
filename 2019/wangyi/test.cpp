09959d2953dff1b23fc096e1833e07e5253058b2
LambdaRan <272045023@qq.com>
2018-08-25 12:07:15 +0800

    20180825

--- 2019/wangyi/test.cpp
+++ 2019/wangyi/test.cpp
@@ -1,54 +1,45 @@
 
 #include <iostream>
+#include <algorithm>
+//#include <sstream>
 #include <vector>
+//#include <string>
+#include <utility>
+#include <map>
 
 using namespace std;
 
-int longest(string s) 
-{
-    int maxLength = 0;
-    int preError = -1;
-    vector<int> oldLetterIndexVec;
-    for(int i = 0; i < s.size(); ++i)
-    {
-        if (s[i] == '(')
-        {
-            oldLetterIndexVec.push_back(i);
-        }
-        else if (s[i] == ')') 
-        {
-            if (oldLetterIndexVec.size() > 0)
-            {
-                oldLetterIndexVec.pop_back();
-                int curLen;
-                if (oldLetterIndexVec.size() == 0)
-                    curLen = i - preError;
-                else 
-                    curLen = i - oldLetterIndexVec.back();
-
-                if (curLen > maxLength) 
-                    maxLength = curLen;
-            }
-            else
-                preError = i;
-        }
-        else 
-        {
-            oldLetterIndexVec.clear();
-            preError = i;
-        }
-    }
-    return maxLength;
-}
-
 int main()
 {
+    int nCap = 0; 
+    size_t tTime = 0;
+    cin >> nCap >> tTime;
+    vector<int> everyPM(nCap);
+    for (int i = 0; i < nCap; ++i)
+        cin >> everyPM[i];
 
-    string str;
-    while (getline(cin, str))
+    std::map<int, size_t> valueCountMap;
+    size_t maxLen = 0;
+    valueCountMap[everyPM[nCap-1]] = 0;
+    for (int i = nCap-2; i >= 0; --i)
     {
-        int result = longest(str);
-        cout << result << endl;
+        if (everyPM[i] <= valueCountMap.rbegin()->first)
+        {
+            size_t mapSize = valueCountMap.size();
+            valueCountMap[everyPM[i]] = mapSize;
+        }
+        else  
+            valueCountMap[everyPM[i]] = 0;
+        maxLen = std::max(maxLen, valueCountMap[everyPM[i]]);
     }
+    if (maxLen < tTime)
+        maxLen = maxLen + tTime - 1;
+    else  
+        maxLen = tTime;
+    cout << maxLen << endl;
     return 0;
 }
+
+// 4 3
+// 10 3 7 5
+// 4
