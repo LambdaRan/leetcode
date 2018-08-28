233af35ef2aaeebd82221a4a5bf2e1846540a769
LambdaRan <2861429309@qq.com>
2018-08-28 20:06:20 +0800

    201808282006

--- 2019/beike/test.cpp
+++ 2019/beike/test.cpp
@@ -5,12 +5,15 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-
+  int a;float b,c;
+  scanf("%2d%3f%4f",&a,&b,&c);
+  printf("\na=%d,b=%d,c=%f\n",a,b,c);
 
     return 0;
 }
