cbcfa09292e3cd586677649ebc66b82eaab35c03
LambdaRan <272045023@qq.com>
2018-09-25 21:10:02 +0800

    2019 add juren

---
+++ 2019/other/prob092501.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <functional>
+#include <utility>
+#include <vector>
+#include <string>
+// #include <sstream>
+// #include <cctype>
+//#include <stdio.h>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
--- 2019/other/test.cpp
+++ 2019/other/test.cpp
@@ -1,52 +1,21 @@
 
 #include <iostream>
 #include <algorithm>
-#include <sstream>
 #include <functional>
-#include <utility>
 #include <vector>
-#include <string>
-#include <sstream>
-#include <cctype>
-//#include <stdio.h>
-
+#include <stdio.h>
 using namespace std;
-
-vector<int> strSplit(string input, char delim) 
-{
-    vector<int> output;
-    // trimLeftTrailingSpaces(input);
-    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-        return !isspace(ch);
-    }));
-    // trimRightTrailingSpaces(input);
-    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-        return !isspace(ch);
-    }).base(), input.end());
-    //input = input.substr(1, input.length() - 2);
-    istringstream ss(input);
-    // ss.str(input);
-    string item;
-    // char delim = ',';
-    while (getline(ss, item, delim)) {
-        output.emplace_back(stoi(item));
-    }
-    return output;
-}
 
 int main()
 {
-    string line;
-    int num;
-    cin >> num;
-    char endd;
-    cin >> endd;
-    for (int i = 0; i < num; ++i)
-    {
-        getline(cin, line);
-        vector<int> ret = strSplit(line, ' ');
-        cout << "size: " << ret.size() << endl;
-    }
-
+    cout << "Hello world" << endl;
+    double darr[2] = {4, 8}, *p , *q;
+    p = &darr[0];
+    q = p + 1;
+    cout << (q-p) << endl;
+    cout << ((int)q - (int)p) << endl;
+    // char* sz = "0123456789";
+    // int *p = (int*)sz;
+    // printf("%x\n", *++p);
     return 0;
 }
