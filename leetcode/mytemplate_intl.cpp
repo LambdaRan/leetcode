f319ae8403c3bae6c08953b590e12be920f10ff9
LambdaRan <2861429309@qq.com>
2018-09-30 21:06:34 +0800

    20180930

--- leetcode/mytemplate_intl.cpp
+++ leetcode/mytemplate_intl.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 /*
-* Name: 118. Pascal's Triangle
-* WebSite: https://leetcode.com/problems/pascals-triangle/description/
+* Name: 
+* WebSite: 
 * Description: 
 *
 */
@@ -21,37 +21,6 @@ public:
     * Runtime:    ms
     * Your runtime beats  % of cpp submissions. 
     */
-    void create(vector<vector<int>>& result, vector<int>& top)
-    {
-        vector<int> newColumns(1,1);
-        for (size_t i = 0; i < top.size()-1; ++i)
-            newColumns.emplace_back(top[i]+top[i+1]);
-        newColumns.emplace_back(1);
-        result.emplace_back(newColumns);
-    }
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
-        switch (numRows)
-        {
-            case 0: 
-                break;
-            case 1: 
-                result.emplace_back(vector<int>(1,1));
-                break;
-            case 2:
-                result.emplace_back(vector<int>(2,1));
-                break;
-            default: 
-            {
-                result.emplace_back(vector<int>(1,1));
-                result.emplace_back(vector<int>(2,1));
-                for (int i = 2; i < numRows; ++i)
-                    create(result, result.back());
-            }
-            break;
-        }  
-        return result;
-    }
 };
 
 int main() 
